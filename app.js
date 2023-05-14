652b4207873fc41cb3ef8b931fb4d22e4bad1be0
azazawejden <azazawejden@gmail.com>
2023-05-14 20:25:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const moment = require('moment');
+
+const app = express();
+
+// Middleware to check if the current time is within working hours
+const checkWorkingHours = (req, res, next) => {
+  const now = moment();
+  const dayOfWeek = now.day();
+  const hour = now.hour();
+
+  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hour >= 9 && hour <= 17) {
+    next();
+  } else {
+    res.send('Sorry, we are only available during working hours (Monday to Friday, from 9 to 17).');
+  }
+};
+
+// Routes
+app.get('/', checkWorkingHours, (req, res) => {
+  res.send('<h1>Home Page</h1>');
+});
+
+app.get('/services', checkWorkingHours, (req, res) => {
+  res.send('<h1>Our Services</h1>');
+});
+
+app.get('/contact', checkWorkingHours, (req, res) => {
+  res.send('<h1>Contact Us</h1>');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
